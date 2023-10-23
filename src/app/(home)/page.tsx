da555a0978322cc214f5880f0fb279d3e642dbd9
jhonathanalencar <jhonathan.alencarfs@gmail.com>
2023-10-23 17:16:02 -0300

    feat: add mouses section to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -23,10 +23,10 @@ export default async function Home() {
     },
   });
 
-  const headphones = await prismaClient.product.findMany({
+  const mouses = await prismaClient.product.findMany({
     where: {
       category: {
-        slug: "headphones",
+        slug: "mouses",
       },
     },
   });
@@ -63,8 +63,8 @@ export default async function Home() {
       />
 
       <div>
-        <SectionTitle>Fones</SectionTitle>
-        <ProductList products={headphones} />
+        <SectionTitle>Mouses</SectionTitle>
+        <ProductList products={mouses} />
       </div>
     </div>
   );
