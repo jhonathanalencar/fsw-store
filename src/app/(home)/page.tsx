899118655331ac4941bf6f6815fba8c3f82337ba
jhonathanalencar <jhonathan.alencarfs@gmail.com>
2023-10-23 16:38:42 -0300

    feat: add second banner to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -29,8 +29,18 @@ export default async function Home() {
       </div>
 
       <div className="mt-8">
+        <p className="mb-3 pl-5 font-semibold uppercase">Ofertas</p>
         <ProductList products={deals} />
       </div>
+
+      <Image
+        src="/banner-home-02.png"
+        alt="Até 55% de desconto em mouses!"
+        height={0}
+        width={0}
+        sizes="100vw"
+        className="h-auto w-full px-5"
+      />
     </div>
   );
 }
