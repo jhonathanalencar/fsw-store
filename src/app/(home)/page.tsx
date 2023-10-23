e4b547f1c501fb9eaee2d9849c8a3fa005519450
jhonathanalencar <jhonathan.alencarfs@gmail.com>
2023-10-23 17:12:53 -0300

    feat: add third banner to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -23,18 +23,26 @@ export default async function Home() {
     },
   });
 
+  const headphones = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: "headphones",
+      },
+    },
+  });
+
   return (
-    <div>
+    <div className="flex flex-col gap-8">
       <PromoBanner
         src="/banner-home-01.png"
         alt="Até 55% de desconto esse mês!"
       />
 
-      <div className="mt-8 px-5">
+      <div className="px-5">
         <Categories />
       </div>
 
-      <div className="mt-8">
+      <div>
         <SectionTitle>Ofertas</SectionTitle>
         <ProductList products={deals} />
       </div>
@@ -44,9 +52,19 @@ export default async function Home() {
         alt="Até 55% de desconto em mouses!"
       />
 
-      <div className="mt-8">
+      <div>
         <SectionTitle>Teclados</SectionTitle>
         <ProductList products={keyboards} />
+      </div>
+
+      <PromoBanner
+        src="/banner-home-03.png"
+        alt="Até 20% de desconto em fones!"
+      />
+
+      <div>
+        <SectionTitle>Fones</SectionTitle>
+        <ProductList products={headphones} />
       </div>
     </div>
   );
