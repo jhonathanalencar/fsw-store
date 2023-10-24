3b1b5ea2397148a3f44e27a85c3cebd8ee6b2b93
jhonathanalencar <jhonathan.alencarfs@gmail.com>
2023-10-24 20:09:45 -0300

    feat: ensure cart products can be scrolled

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -4,13 +4,15 @@ import { useContext } from "react";
 import { CartContext } from "@/providers/cart";
 import { CartItem } from "./cart-item";
 import { Separator } from "./separator";
+import { ScrollArea } from "./scroll-area";
+import { Button } from "./button";
 
 export const Cart = () => {
   const { products, cartBasePrice, cartTotalDiscount, cartTotalPrice } =
     useContext(CartContext);
 
   return (
-    <div className="flex flex-col gap-8">
+    <div className="flex h-full flex-col gap-8">
       <Badge
         className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
         variant="outline"
@@ -19,16 +21,18 @@ export const Cart = () => {
         Carrinho
       </Badge>
 
-      <div className="flex flex-col gap-5">
-        {products.length > 0 ? (
-          products.map((product) => (
-            <CartItem key={product.id} product={product} />
-          ))
-        ) : (
-          <p className="text-center font-semibold">
-            Carrinho vazio. Vamos fazer compras?
-          </p>
-        )}
+      <div className="flex flex-1 flex-col gap-5 overflow-hidden">
+        <ScrollArea className="h-full">
+          {products.length > 0 ? (
+            products.map((product) => (
+              <CartItem key={product.id} product={product} />
+            ))
+          ) : (
+            <p className="text-center font-semibold">
+              Carrinho vazio. Vamos fazer compras?
+            </p>
+          )}
+        </ScrollArea>
       </div>
 
       <div className="flex flex-col gap-3">
@@ -59,6 +63,8 @@ export const Cart = () => {
           <p>Total</p>
           <p>R$ {cartTotalPrice.toFixed(2)}</p>
         </div>
+
+        <Button className="mt-7 font-bold uppercase">Finalizar compra</Button>
       </div>
     </div>
   );
