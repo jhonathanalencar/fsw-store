e2419ab6497174944df8e93e927f8e4870d49be2
jhonathanalencar <jhonathan.alencarfs@gmail.com>
2023-10-25 00:29:00 -0300

    fix: add missing import

--- src/helpers/convert-product-to-plain-object.ts
+++ src/helpers/convert-product-to-plain-object.ts
@@ -1,4 +1,5 @@
 import { Product } from "@prisma/client";
+import { PlainProduct } from "./product";
 
 export function convertProductToPlainObject(product: Product): PlainProduct {
   const { basePrice, ...rest } = product;
